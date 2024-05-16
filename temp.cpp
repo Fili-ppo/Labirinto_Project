b8f214d194799f4370ad57c833bfcec28cf33588
Fili-ppo <filip.ruffoni@gmail.com>
2024-05-16 18:30:03 +0200

    COMMIT_EDITMSG

--- temp.cpp
+++ temp.cpp
@@ -3,8 +3,12 @@
 #include "personaggio.h"
 int main(void){
     Personaggio p;
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Joystick", sf::Style::Fullscreen);
-    p.crea("zelda.png");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML Joystick");
+    p.crea("Images/personaggioDx.png");
+    sf::Texture t;
+    t.loadFromFile("Images/stanza1.png");
+    sf::Sprite sfondo;
+    sfondo.setTexture(t);
     while (window.isOpen())
     {
         sf::Event event;
@@ -19,9 +23,10 @@ int main(void){
             float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
             p.sprite.move(x * 0.01f, y * 0.01f);
         }
-        window.clear();
+        window.clear(sf::Color::White);
+        window.draw(sfondo);
         window.draw(p.sprite);
         window.display();
     }
     return 0;
-}
+} 
