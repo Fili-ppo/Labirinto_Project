3ae5c855f7d74daaf67052e49601cec089b57f2e
Fili-ppo <filip.ruffoni@gmail.com>
2024-05-17 09:22:12 +0200

    Co-authored-by: laura.moscheni06@gmail.com <laura.moscheni06@gmail.com>

--- casella.cpp
+++ casella.cpp
@@ -1,1 +1,4 @@
 #include "casella.h"
+Casella::Casella(){
+
+}
--- casella.h
+++ casella.h
@@ -1,6 +1,11 @@
 #ifndef CASELLA_H
 #define CASELLA_H
-
+#include "Personaggi/aiutante.h"
+#include "Personaggi/casino.h"
+#include "Personaggi/medico.h"
+#include "Personaggi/quiz.h"
+#include "Personaggi/nemico.h"
+#include "Personaggi/mercante.h"
 class Casella{
     private:
         bool su;
@@ -8,8 +13,22 @@ class Casella{
         bool destra;
         bool sinistra;
         bool mobile;
+        bool haMercato;
+        bool haOspedale;
+        bool haCassa;
+        bool haNemici;
+        bool haQuiz;
+        bool haAiutante;
+        bool haCasino;
+        int numNemici;
     public:
         Casella();
+        Aiutante a;
+        Casino c;
+        Medico m;
+        Quiz q;
+        Nemico n;
+        Mercante me;
         
 };
 #endif
