02c0a0e250bd8023a15375430ca75ade8b1a02e3
Fili-ppo <filip.ruffoni@gmail.com>
2024-05-22 13:40:32 +0200

    COMMITEDIT_MSG

--- cassa.cpp
+++ cassa.cpp
@@ -1,4 +1,15 @@
 #include "cassa.h"
+#include <iostream>
 //Aiutante::Aiutante(){
 
 //}
+int Cassa::interazione(Personaggio *p){
+    if(Keyboard::isKeyPressed(Keyboard::E)){
+        p->setGemme(p->getGemme()+25);
+        std::cout<<"ciao";
+        textureAperta.loadFromFile("Images\\cassaAperta.png");
+        sprite.setTexture(textureAperta);
+        giaInteragito=true;
+    } 
+    return 0;
+}
--- cassa.h
+++ cassa.h
@@ -6,7 +6,12 @@ class Cassa:public NPC{
 
 
     public:
-        Cassa():NPC(){texture->loadFromFile("Images\\cassa.png"); sprite.setTexture(*texture); sprite.setScale(0.5,0.5); sprite.setPosition(500-85, 500-85);};
+        Cassa():NPC(){texture->loadFromFile("Images\\cassa.png"); sprite.setTexture(*texture); sprite.setScale(0.5,0.5); sprite.setPosition(500-85, 500-85);
+                      /*textureE.loadFromFile("Images\\tastoE.png"); tasto.setTexture(textureE); tasto.setScale(0.2, 0.2); tasto.setPosition(200, 200);*/};
         int interazione(Personaggio *p){return 0;};
+        Texture textureAperta;
+        //Texture textureE;
+        //Sprite tasto;
+
 };
 #endif
